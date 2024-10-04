b454e4b24946aaa44644e4d5ace06bbe5fdd68b2
Zihan Yang <zihan_yang_1996@outlook.com>
2024-10-03 20:25:32 -0400

    Add a custom BT Service for calculating the distance between the boss and the player.

---
+++ Source/ActionCombat/Private/Characters/BTS_PlayerDistance.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/BTS_PlayerDistance.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "GameFramework/Character.h"
+
+
+UBTS_PlayerDistance::UBTS_PlayerDistance()
+{
+	NodeName = TEXT("Player Distance");
+	bNotifyTick = true;
+	bNotifyBecomeRelevant = true;
+}
+
+void UBTS_PlayerDistance::OnBecomeRelevant(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	PlayerCharacter = GetWorld()->GetFirstPlayerController()->GetCharacter();
+}
+
+void UBTS_PlayerDistance::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	if (!PlayerCharacter)
+	{
+		UE_LOG(LogTemp, Error, TEXT("PlayerCharacter is nullptr!"));
+		return;
+	}
+	FVector PlayerLocation{PlayerCharacter->GetActorLocation()};
+	float DistanceToPlayer{
+		static_cast<float>(FVector::Distance(PlayerLocation, OwnerComp.GetAIOwner()->GetPawn()->GetActorLocation()))
+	};
+
+	OwnerComp.GetBlackboardComponent()->SetValueAsFloat(TEXT("DistanceToPlayer"), DistanceToPlayer);
+}
